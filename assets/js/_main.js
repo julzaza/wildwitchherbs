fb19e1a5c821a4f6fe04793af48f1469e2d866ce
julzaza <juliamolton@gmail.com>
2026-02-28 19:29:59 +0100

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,4 +1,62 @@
 /* ==========================================================================
+   GREEDY NAVIGATION - Mobile Menu Toggle
+   ========================================================================== */
+
+$(document).ready(function() {
+  var $nav = $('#site-nav');
+  var $btn = $('button.greedy-nav__toggle');
+  var $vlinks = $('.visible-links');
+  var $hlinks = $('.hidden-links');
+  
+  var numOfItems = 0;
+  var totalSpace = 0;
+  var breakWidths = [];
+  
+  // Get initial state
+  $vlinks.children().outerWidth(function(i, w) {
+    totalSpace += w;
+    numOfItems += 1;
+    breakWidths.push(totalSpace);
+  });
+  
+  var availableSpace, numOfVisibleItems, requiredSpace;
+  
+  function check() {
+    availableSpace = $vlinks.width() - 10;
+    numOfVisibleItems = $vlinks.children().length;
+    requiredSpace = breakWidths[numOfVisibleItems - 1];
+    
+    if (requiredSpace > availableSpace) {
+      $vlinks.children().last().prependTo($hlinks);
+      numOfVisibleItems -= 1;
+      check();
+    } else if (availableSpace > breakWidths[numOfVisibleItems]) {
+      $hlinks.children().first().appendTo($vlinks);
+      numOfVisibleItems += 1;
+    }
+    
+    if (numOfVisibleItems === numOfItems) {
+      $btn.addClass('hidden');
+    } else {
+      $btn.removeClass('hidden');
+    }
+  }
+  
+  // Window resize
+  $(window).resize(function() {
+    check();
+  });
+  
+  // Toggle click - THIS MAKES THE HAMBURGER WORK
+  $btn.on('click', function() {
+    $hlinks.toggleClass('hidden');
+    $(this).toggleClass('close');
+  });
+  
+  check();
+});
+
+/* ==========================================================================
    jQuery plugin settings and other scripts
    ========================================================================== */
 
