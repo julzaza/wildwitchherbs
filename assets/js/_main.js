18935eac313535a71b78c6be6121f7fdf0b626ba
julzaza <juliamolton@gmail.com>
2026-03-01 12:57:36 +0100

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,54 +1,31 @@
-/* ==========================================================================
-   GREEDY NAVIGATION - Mobile Menu Only
-   ========================================================================== */
-
-$(document).ready(function() {
-  var $nav = $('#site-nav');
-  var $btn = $('button.greedy-nav__toggle');
-  var $vlinks = $('.visible-links');
-  var $hlinks = $('.hidden-links');
+// Minimal JS - only greedy navigation
+(function() {
+  'use strict';
   
-  var numOfItems = 0;
-  var totalSpace = 0;
-  var breakWidths = [];
-  
-  $vlinks.children().outerWidth(function(i, w) {
-    totalSpace += w;
-    numOfItems += 1;
-    breakWidths.push(totalSpace);
-  });
-  
-  var availableSpace, numOfVisibleItems, requiredSpace;
-  
-  function check() {
-    availableSpace = $vlinks.width() - 10;
-    numOfVisibleItems = $vlinks.children().length;
-    requiredSpace = breakWidths[numOfVisibleItems - 1];
+  // Wait for DOM to be ready
+  function init() {
+    var btn = document.querySelector('.greedy-nav__toggle');
+    var hiddenLinks = document.querySelector('.hidden-links');
     
-    if (requiredSpace > availableSpace) {
-      $vlinks.children().last().prependTo($hlinks);
-      numOfVisibleItems -= 1;
-      check();
-    } else if (availableSpace > breakWidths[numOfVisibleItems]) {
-      $hlinks.children().first().appendTo($vlinks);
-      numOfVisibleItems += 1;
+    if (!btn || !hiddenLinks) {
+      console.log('Navigation elements not found');
+      return;
     }
     
-    if (numOfVisibleItems === numOfItems) {
-      $btn.addClass('hidden');
-    } else {
-      $btn.removeClass('hidden');
-    }
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      hiddenLinks.classList.toggle('hidden');
+      this.classList.toggle('close');
+      console.log('Menu toggled');
+    });
+    
+    console.log('Navigation initialized');
   }
   
-  $(window).resize(function() {
-    check();
-  });
-  
-  $btn.on('click', function() {
-    $hlinks.toggleClass('hidden');
-    $(this).toggleClass('close');
-  });
-  
-  check();
-});
+  // Run when DOM is ready
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
